3e7bf2af6cc056397be6204ce83541a7a9476153
Furkan Öztürk <furkan.ozturk@axi.nl>
2022-05-09 11:14:18 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
+import Tasks from './components/Tasks';
+import DAOs from './components/DAOs';
+import Stake from './components/Stake';
+import Dashboard from './components/Dashboard';
+
+import React from 'react';
+import { Routes, Link as ReactLink, Route } from 'react-router-dom';
+import { Grid, Card, Input, Button, Text, Spacer, Link } from "@nextui-org/react";
 import './App.css';
+
+import { ListBulletIcon, DashboardIcon, LayersIcon, AngleIcon } from '@radix-ui/react-icons'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid.Container className="container">
+
+      {/* Topbar */}
+      <Grid xs={12}>
+        <Grid.Container className="topnav" justify="space-between" alignItems="center">
+          <Grid >
+            <a href="#Dashboard">Bounti</a>
+          </Grid>
+          <Grid >
+            <Grid.Container gap={2}>
+              <Grid>
+                <Input labelPlaceholder="Search" />
+              </Grid>
+              <Grid>
+                <Button>Account</Button>
+              </Grid>
+
+            </Grid.Container>
+          </Grid>
+        </Grid.Container>
+      </Grid>
+
+      {/* Sidebar */}
+      <Grid xs={1} className="sidenav">
+        <ul>
+          <Link>
+            <Text wrap color css={{ color: "white", }}>
+              <li><ReactLink to='/'>
+                <DashboardIcon />
+                Dashboard
+              </ReactLink></li>
+            </Text>
+            <Spacer y={2} />
+          </Link>
+
+
+          <Text color css={{ color: "white", }}>
+            <li><ReactLink to='/DAOs'>
+              <LayersIcon />
+              DAOs
+            </ReactLink></li>
+          </Text>
+          <Spacer y={2} />
+
+
+          <Text color css={{ color: "white", }}>
+            <li><ReactLink to='Tasks'>
+              <ListBulletIcon />
+              Tasks
+            </ReactLink></li>
+          </Text>
+          <Spacer y={2} />
+
+
+          <Text color css={{ color: "white", }}>
+            <li><ReactLink to='Stake'>
+              <AngleIcon />
+              Stake
+              <Spacer y={2} />
+            </ReactLink></li>
+          </Text>
+        </ul>
+      </Grid>
+
+      {/* Content */}
+      <Grid xs={11}>
+        <Card>
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/tasks' element={<Tasks />} />
+            <Route path='/stake' element={<Stake />} />
+            <Route path='/daos' element={<DAOs />} />
+          </Routes>
+
+
+        </Card>
+      </Grid>
+
+    </Grid.Container >
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { createTheme, NextUIProvider } from "@nextui-org/react"
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const theme = createTheme({
+  type: "default",
+  theme: {
+    colors: {
+      gradient: 'linear-gradient(90deg, rgba(131,83,127,1) 0%, rgba(211,118,136,1) 100%)',
+      // you can also create your own color
+      myColor: '#ff4ecd'
+
+      // ...  more colors
+    },
+    space: {},
+    fonts: {}
+  }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <BrowserRouter>
+    <NextUIProvider theme={theme}>
+      <App />
+    </NextUIProvider>
+  </BrowserRouter>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+);
