6fce3533ede51f6023a58bf5721c7cb0952d9e27
Apšega,Ignas I <i.apsega@student.fontys.nl>
2022-06-09 02:05:39 +0200

    start of completeTask function

---
+++ src/contracts/Daos.sol
@@ -0,0 +1,154 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ */
+
+contract Daos {
+    struct Dao {
+        //Every DAO has address & list of members
+        address contractAddress;
+        address[] membersArray; //To index
+        //Taken away to allow getAllDaos
+        //mapping(address => address) membersMapping; //Used mapping to be able ot search for users by address
+    }
+
+    //Bounti has a list of DAOs
+    address[] daosArray; //To get all DAO contract address
+    mapping(address => Dao) daosMap; //to search by DAO and get members
+
+    /**
+     * @dev Adding DAO to bounti and also adds msg.sender to dao automatically
+     */
+    function addDao(address dao) public returns (bool success) {
+        dao = daosArray.push();
+        Dao storage _newDao = daosMap[dao];
+        _newDao.contractAddress = dao;
+        daosMap[dao].membersArray.push(msg.sender); //adds user that added dao to said dao
+        return true;
+    }
+
+    /**
+     * @dev Gets an array of DAOs
+     * This only gets array of DAO contracts unable to get members per dao
+     */
+    function getAllDaos() public view returns (address[] memory) {
+        return daosArray;
+    }
+
+    /**
+     * @dev Get dao from provided contract address
+     * gets DAO contract and most recent version of membersArray
+     */
+    function getDao(address dao) public view returns (Dao memory) {
+        return daosMap[dao];
+    }
+
+    /**
+     * @dev Removes DAO both from Array and Map
+     */
+    function removeDao(address dao) public returns (bool succes) {
+        for (uint256 i = 0; i < daosArray.length; i++) {
+            if (daosArray[i] == dao) {
+                delete daosArray[i];
+            }
+        }
+        delete daosMap[dao];
+        return true;
+    }
+
+    /**
+     * @dev Check if member exists in DAO
+     */
+    function checkMember(address dao, address member)
+        public
+        view
+        returns (bool success)
+    {
+        for (uint256 i = 0; i < daosMap[dao].membersArray.length; i++) {
+            if (daosMap[dao].membersArray[i] == member) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    /**
+     * @dev User joins DAO from given contract
+     * Only updates members list within daoMap
+     */
+    function joinDao(address dao) public returns (bool success) {
+        daosMap[dao].membersArray.push(msg.sender);
+        return true;
+    }
+
+    /**
+     * @dev Get all members from a certain DAO
+     */
+    function getAllMembers(address dao) public view returns (address[] memory) {
+        return daosMap[dao].membersArray;
+    }
+
+    /**
+     * @dev Get members number from a certain DAO
+     */
+    function getMembersNumber(address dao) external view returns (uint256) {
+        return daosMap[dao].membersArray.length;
+    }
+
+    /**
+     * @dev Remove member from DAO
+     */
+    function removeMember(address dao, address member)
+        public
+        returns (bool success)
+    {
+        for (uint256 i = 0; i < daosMap[dao].membersArray.length; i++) {
+            if (daosMap[dao].membersArray[i] == member) {
+                delete daosMap[dao].membersArray[i];
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
+// SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.0;
+// // NOTE: Deploy this contract first
+// contract B {
+//     struct Dao{
+//         //Every DAO has address & list of members
+//         address contractAddress;
+//         address[] membersArray; //To index
+//         //Taken away to allow getAllDaos
+//         //mapping(address => address) membersMapping; //Used mapping to be able ot search for users by address
+//     }
+
+//     //Bounti has a list of DAOs
+//     address[] daosArray; //To get all DAO contract address
+//     mapping(address => Dao) daosMap; //to search by DAO and get members
+
+//         /**
+//      * @dev Get members number from a certain DAO
+//      */
+//     function getMembersNumber(address dao) external view returns(uint256) {
+//         return daosMap[dao].membersArray.length;
+//     }
+//     function joinDao(address dao) public returns(bool success) {
+//         daosMap[dao].membersArray.push(msg.sender);
+//         return true;
+//     }
+// }
+
+// contract A {
+//     function getMembersNumber(B _contract, address dao) external view returns (uint256){
+//         // A's storage is set, B is not modified.
+//         return _contract.getMembersNumber(dao);
+//     }
+// }
--- src/contracts/Proposals.sol
+++ src/contracts/Proposals.sol
@@ -1,20 +1,32 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: UNLICENSED
 
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.0;
 
 /**
  * @title Storage
  * @dev Store & retrieve value in a variable
  * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
  */
+
+import "./Daos.sol";
+
 contract Proposals {
-    //do proposal status, check tasks
+    Daos public d;
+
+    enum ProposalStatus {
+        IN_PROCESS,
+        COMPLETED
+    }
+
     struct Proposal {
         address daoContractAddress;
         address owner;
         string id;
         Voter[] votersFor;
         Voter[] votersAgainst;
+        bool initialized;
+        ProposalStatus status;
+        uint256 completeThreshold;
     }
 
     struct Voter {
@@ -24,15 +36,13 @@ contract Proposals {
 
     mapping(string => Proposal) public proposalsMap;
 
-    //mapping(string => mapping(address => bool))
-
     string[] public proposalsArray;
 
-    //2nd parameter voter address, so we could use this in getvoter
-    modifier checkVoter(string calldata id) {
+    //checks if the Voter already exists in the particular Proposal
+    modifier checkVoterExists(string calldata id, address voterAddress) {
         bool voterExists = false;
         for (uint256 i = 0; i < proposalsMap[id].votersFor.length; i++) {
-            if (proposalsMap[id].votersFor[i].voterAddress == msg.sender) {
+            if (proposalsMap[id].votersFor[i].voterAddress == voterAddress) {
                 voterExists = true;
                 _;
                 break;
@@ -45,7 +55,8 @@ contract Proposals {
                 i++
             ) {
                 if (
-                    proposalsMap[id].votersAgainst[i].voterAddress == msg.sender
+                    proposalsMap[id].votersAgainst[i].voterAddress ==
+                    voterAddress
                 ) {
                     voterExists = true;
                     _;
@@ -58,6 +69,24 @@ contract Proposals {
         }
     }
 
+    //checks if the Voter already voted in the particular Proposal
+    modifier checkVoterVoted(string calldata id, address voterAddress) {
+        for (uint256 i = 0; i < proposalsMap[id].votersFor.length; i++) {
+            if (proposalsMap[id].votersFor[i].voterAddress == voterAddress) {
+                revert("Voter already voted");
+            }
+        }
+        for (uint256 i = 0; i < proposalsMap[id].votersAgainst.length; i++) {
+            if (
+                proposalsMap[id].votersAgainst[i].voterAddress == voterAddress
+            ) {
+                revert("Voter already voted");
+            }
+        }
+        _;
+    }
+
+    //checks if msg.sender is proposal owner
     modifier checkProposalOwner(string calldata objectId) {
         if (msg.sender == proposalsMap[objectId].owner) {
             _;
@@ -66,12 +95,57 @@ contract Proposals {
         }
     }
 
+    //checks if proposal exists
+    modifier checkProposalExists(string calldata objectId) {
+        require(
+            proposalsMap[objectId].initialized,
+            "The proposal does not exists"
+        );
+        _;
+    }
+
+    //checks if proposal doesnt exists
+    modifier proposalDoesntExists(string calldata objectId) {
+        require(
+            !proposalsMap[objectId].initialized,
+            "The proposal already exists"
+        );
+        _;
+    }
+
+    //checks if proposal exists
+    modifier emptyProposal(string calldata objectId) {
+        if (compareStrings(objectId, "")) {
+            revert("You have not entered proposal Id");
+        } else {
+            _;
+        }
+    }
+
+    modifier checkProposalStatus(
+        string calldata objectId,
+        ProposalStatus status
+    ) {
+        require(
+            proposalsMap[objectId].status == status,
+            "You can't do this action at current Proposal stage"
+        );
+        _;
+    }
+
     //create modifier if the proposal already exists
-    function createProposal(string calldata objectId) public {
+    function createProposal(string calldata objectId, uint256 completeThreshold)
+        public
+        proposalDoesntExists(objectId)
+        emptyProposal(objectId)
+    {
         proposalsArray.push(objectId);
         Proposal storage _newProposal = proposalsMap[objectId];
         _newProposal.owner = msg.sender;
         _newProposal.id = objectId;
+        _newProposal.initialized = true;
+        _newProposal.status = ProposalStatus.IN_PROCESS;
+        _newProposal.completeThreshold = completeThreshold;
         _newProposal.votersFor.push(
             Voter({voterAddress: msg.sender, weight: 1})
         );
@@ -84,27 +158,44 @@ contract Proposals {
     function getProposal(string calldata objectId)
         public
         view
+        checkProposalExists(objectId)
         returns (Proposal memory)
     {
         return proposalsMap[objectId];
     }
 
+    //contract
+    function completeProposal(
+        address _contract,
+        string calldata objectId,
+        address dao
+    )
+        public
+        checkProposalExists(objectId)
+        checkProposalOwner(objectId)
+        checkProposalStatus(objectId, ProposalStatus.COMPLETED)
+    {
+        uint256 allMembers = _contract.getMembersNumber(dao);
+        proposalsMap[objectId].status = ProposalStatus.COMPLETED;
+    }
+
     function removeProposal(string calldata objectId)
         public
+        checkProposalExists(objectId)
         checkProposalOwner(objectId)
     {
         delete proposalsMap[objectId];
         for (uint256 i = 0; i < proposalsArray.length; i++) {
-            if (proposalsArray[i] == objectId) {
+            if (compareStrings(proposalsArray[i], objectId)) {
                 delete proposalsArray[i];
             }
         }
     }
 
-    //check if the person is already a voter in this proposal
     function getVoter(string calldata objectId, address voterAddress)
         public
         view
+        checkVoterExists(objectId, voterAddress)
         returns (Voter memory voter)
     {
         for (uint256 i = 0; i < proposalsMap[objectId].votersFor.length; i++) {
@@ -127,31 +218,50 @@ contract Proposals {
         }
     }
 
-    function voteFor(string calldata objectId) public checkVoter(objectId) {
+    function voteFor(string calldata objectId)
+        public
+        checkVoterVoted(objectId, msg.sender)
+        checkProposalExists(objectId)
+    {
         proposalsMap[objectId].votersFor.push(
             Voter({voterAddress: msg.sender, weight: 1})
         );
     }
 
-    function voteAgainst(string calldata objectId) public checkVoter(objectId) {
+    function voteAgainst(string calldata objectId)
+        public
+        checkVoterVoted(objectId, msg.sender)
+        checkProposalExists(objectId)
+    {
         proposalsMap[objectId].votersAgainst.push(
             Voter({voterAddress: msg.sender, weight: 1})
         );
     }
 
-    function voteForCount(string calldata objectId)
+    function votedForCount(string calldata objectId)
         public
         view
+        checkProposalExists(objectId)
         returns (uint256)
     {
         return proposalsMap[objectId].votersFor.length;
     }
 
-    function voteAgainstCount(string calldata objectId)
+    function votedAgainstCount(string calldata objectId)
         public
         view
+        checkProposalExists(objectId)
         returns (uint256)
     {
-        return proposalsMap[objectId].votersFor.length;
+        return proposalsMap[objectId].votersAgainst.length;
+    }
+
+    function compareStrings(string memory a, string memory b)
+        private
+        pure
+        returns (bool)
+    {
+        return (keccak256(abi.encodePacked((a))) ==
+            keccak256(abi.encodePacked((b))));
     }
 }
